009edd8c115620c2256ad6fec68df6b4c9ed934b
Leny <leny.levant95@icloud.com>
2025-06-12 00:35:19 +0200

    Modification de l'importation de cheerio pour utiliser l'importation nommée.

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import { CookieJar } from 'tough-cookie';
 import { wrapper } from 'axios-cookiejar-support';
 
